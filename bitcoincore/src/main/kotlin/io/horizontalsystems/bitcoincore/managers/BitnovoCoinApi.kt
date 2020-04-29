f0bc9e59210b6501971a8e1a98406e7837115475
Jose Molina Colmenero <jose@smsdata.es>
2020-04-29 12:05:18 +0200

    Implement wallet-manager parsing logic

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/managers/BitnovoCoinApi.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/managers/BitnovoCoinApi.kt
@@ -2,6 +2,7 @@ package io.horizontalsystems.bitcoincore.managers
 
 import android.util.Base64
 import com.eclipsesource.json.Json
+import com.eclipsesource.json.JsonArray
 import com.eclipsesource.json.JsonObject
 import io.horizontalsystems.bitcoincore.core.IInitialSyncApi
 import io.horizontalsystems.bitcoincore.utils.HashUtils
@@ -25,37 +26,44 @@ class BitnovoCoinApi(
         val maxSize = 20
         while (current < addresses.size) {
             val size = min(maxSize, addresses.size - current)
-            val currentAddresses = addresses.slice(current until size)
+            val limit = current + size
+            val currentAddresses = addresses.slice(current until limit)
             val requestData = JsonObject().apply {
                 this["addresses"] = Json.array(*currentAddresses.toTypedArray())
             }
             val response = apiManager.post("api/v1/wallets/$coin/tx/", requestData.toString(), authToken()).asArray()
 
             logger.info("Got ${response.size()} transactions for requested addresses")
-
-            for (txItem in response) {
-                val tx = txItem.asObject()
-
-                val blockHashJson = tx["block"] ?: continue
-                val blockHash = if (blockHashJson.isString) blockHashJson.asString() else continue
-
-                val outputs = mutableListOf<TransactionOutputItem>()
-                for (outputItem in tx["outputs"].asArray()) {
-                    val outputJson = outputItem.asObject()
-
-                    val scriptJson = outputJson["script"] ?: continue
-                    val addressJson = outputJson["address"] ?: continue
-
-                    if (scriptJson.isString && addressJson.isString) {
-                        outputs.add(TransactionOutputItem(scriptJson.asString(), addressJson.asString()))
-                    }
-                }
-
-                transactions.add(TransactionItem(blockHash, tx["height"].asInt(), outputs))
-            }
+            processTransactions(response, transactions)
             current += size
         }
         return transactions
+    }
+
+    private fun processTransactions(response: JsonArray, transactions: MutableList<TransactionItem>) {
+        for (txItem in response) {
+            val tx = txItem.asObject()
+
+            val blockHashJson = tx["blockHash"] ?: continue
+            val blockHash = if (blockHashJson.isString) blockHashJson.asString() else continue
+
+            val outputs = mutableListOf<TransactionOutputItem>()
+            for (outputItem in tx["outputs"].asArray()) {
+                val outputJson = outputItem.asObject()
+
+                val scriptJson = outputJson["script"] ?: continue
+                val addresses = outputJson["addresses"] ?: continue
+
+                if (scriptJson.isString) {
+                    for (address in addresses.asArray()) {
+                        val addressJson = if (address.isString) address.asString() else continue
+                        outputs.add(TransactionOutputItem(scriptJson.asString(), addressJson))
+                    }
+                }
+            }
+
+            transactions.add(TransactionItem(blockHash, tx["blockHeight"].asInt(), outputs))
+        }
     }
 
     private fun authToken(): String {
